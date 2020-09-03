6a4c317b2d9c5602626b39ada9c53ce13abf22ea
Mike <mikedimo1@gmail.com>
2020-09-03 00:37:49 -0400

    ChallengeSet1 - All Together

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,32 +5,47 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            return num1 == num2;
+            //throw new NotImplementedException();
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
+            //throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else if (number2 > number1)
+            {
+                return number1;
+            }
+            return number1;
+            //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+            //throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return (nameOfPerson == "") ? ("Hello!") : ($"Hello, {nameOfPerson}!");
+
+            //throw new NotImplementedException();
         }
 
         public string GetHey()
